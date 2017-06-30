86409bf91a5dbea7c8007cecb2f85cc03ba07f62
Naookie Sato <necrosato@live.com>
2017-06-30 16:32:27 -0700

    week2 assignment folder and start

---
+++ week2/instanceN/cmds.sh
@@ -0,0 +1,25 @@
+#3 Provide the output of a command showing how much disk space one of your instances has.
+echo "df"
+echo "df -h"
+#4 Provide the output of a command showing how much memory one of your instances has.
+echo "free"
+echo "free -h"
+#5 Provide a listing showing all the processes running in one of your instances.
+echo "ps -a"
+echo "ps -A"
+#6 Provide the output of a command showing the network interface configuration of your instance (this should show its private IP address).
+echo "ifconfig"
+echo "ifconfig -a"
+#7 Provide the output of a command showing the routing table of your instance.
+#echo "route -v" 
+#or
+echo "netstat -r"
+#8 Provide the output of a command showing all of the open network connections and ports on one of your instances.
+echo "netstat -a"
+#9 Provide a listing of all of the software packages installed on one your instances.
+echo "dpkg --list"
+#10 Provide transcripts showing the output of the commands you use to update your package database and upgrade packages on each of your instances.
+#update package dependencies
+echo "sudo apt-get update"
+#get updated packages
+echo "sudo apt-get upgrade"
