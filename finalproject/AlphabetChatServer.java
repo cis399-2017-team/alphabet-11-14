a6292891f0b77dd098474b857c2f9199314f40cb
Naookie Sato <nsato@cs.uoregon.edu>
2017-08-04 14:42:39 +0000

    disaster avoided

---
+++ finalproject/AlphabetChatServer.java
@@ -0,0 +1,26 @@
+import java.net.*;
+import java.io.*;
+import java.awt.*;
+import java.util.*;
+
+public class AlphabetChatServer {
+
+    public static String server_host = "10.0.6.22";
+    public static int server_port = 10000;
+
+    public AlphabetChatServer(String ip, int port) throws IOException {
+        ServerSocket server = new ServerSocket();
+        server.bind(new InetSocketAddress(ip, port));
+        System.out.println("server socket created, inet address: " + server.getInetAddress());
+        while (true) {
+            Socket client = server.accept();
+            System.out.println("Accepted from " + client.getInetAddress());
+            AlphabetChatThread c = new AlphabetChatThread(client.getInetAddress().toString(), client);
+            c.start();
+        }
+    }
+
+    public static void main(String args[]) throws IOException {
+        new AlphabetChatServer(server_host, server_port);
+    }
+}
